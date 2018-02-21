ae7a8a14c53fd02572bd5b661786d081890c34bf
Shoeb Md Moniruzzaman <shoeb234abc@gmail.com>
2018-02-21 13:10:21 +0600

    updated

--- js/states/HomeState.js
+++ js/states/HomeState.js
@@ -18,18 +18,20 @@ var HomeState = {
       align: 'center'
     };
 
-    this.btn_play= this.game.add.button(this.game.world.centerX, 80, 'btn_play');
+    this.btn_play= this.game.add.button(this.game.world.centerX, 140, 'btn_play');
     this.btn_play.anchor.setTo("0.5");
     this.btn_play.events.onInputOver.add(this.startGame, this);
     this.btn_play.events.onInputDown.add(this.startGame, this);    
     
-    this.btn_learn= this.game.add.button(this.game.world.centerX, 220, 'btn_learn');
+    this.btn_learn= this.game.add.button(this.game.world.centerX, 270, 'btn_learn');
     this.btn_learn.anchor.setTo("0.5");
     this.btn_learn.events.onInputOver.add(this.startLearnState, this);
     this.btn_learn.events.onInputDown.add(this.startLearnState, this);   
 
-    this.btn_bonus= this.game.add.button(this.game.world.centerX, 360, 'btn_bonus');
+    /*this.btn_bonus= this.game.add.button(this.game.world.centerX, 360, 'btn_bonus');
     this.btn_bonus.anchor.setTo("0.5");
+    this.btn_bonus.events.onInputOver.add(this.startBonusState, this);
+    this.btn_bonus.events.onInputDown.add(this.startBonusState, this);*/   
 
   },
   startGame: function(){
@@ -37,5 +39,8 @@ var HomeState = {
   },
   startLearnState: function(){
     this.game.state.start('LearnState');
+  },
+  startBonusState: function(){
+    window.location.href = "bonus.html";
   }
 };
